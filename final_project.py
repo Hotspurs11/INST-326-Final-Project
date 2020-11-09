c325d520f546335c7a343e72373d2eb78137b6e3
varun <mailme@varun-sharma.com>
2020-11-09 12:23:57 -0500

    this is my part of the project

---
+++ final_project.py
@@ -0,0 +1,23 @@
+class Dealer:
+    """This class is responsble for dealing out the acrds to the players in the game. It should be able to deal to both human and computer players. this class will contain the deal function and the shuffle fucntion. The deal function will draw 2 cards
+    for the dealer and deal 2 cards to the other player. The shuffle fucntion will shuffle the 52 card deck including the jokers to ensure randomness.
+    Args: N/A
+    Returns: 2 cards to each player 
+    Raises: N/A
+    Side Effects: N/A"""
+    
+    def deal():
+        """this fucntion will be rsponsible for drawing and dealing the 2 cards for each player. it will take cards from the shuffled cards.
+        Args: N/A
+        Returns: 2 cards for each player playing (4 cards in total)
+        Raises:N/A
+        Side effects: N/A"""
+        
+    def shuffle():
+        """This funciton is responsible for shuffling the cards each game so that the game remians fair and random. This function will make sure to include all 52 cards including the joker. we will import the shuffle fucntion from random
+        Args: shuffle 
+        Returns: a deck of 52 cards that are shuffled
+        Raises: N/A
+        Side effects: N/A
+    
+    
