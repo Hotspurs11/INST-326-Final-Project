274bf336e7c8ff9e418c116ba1d78ad648ff6af4
nayabmahmood <73296023+nayabmahmood@users.noreply.github.com>
2020-11-10 14:00:13 -0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,22 @@
+def update():
+    """
+    store users scores from previous functions and intialize the balances of each player
+    args:
+        player1_score(int): the value of the score of the dealer from previous parts
+        player2_score(int): the value of the score of the player
+        
+    return:
+        int:  users current balance
+    """
+
+def endgame():
+    """
+    Compares the hand value to dealer value and presents a winner of the overall game
+    args:  
+        hand(int): stores hand value to dealer value    
+    returns:
+       list of str:  representation of the end balance of the player, indicaiting winner of game
+    Side Effects: 
+        modifies score   
+       
+    """
