a11a78adefd8e1abbdefe895ab2a54c0752545d5
pvikram101 <73295773+pvikram101@users.noreply.github.com>
2020-12-03 19:01:22 -0500

    Update group_project_docstrings.py

--- group_project_docstrings.py
+++ group_project_docstrings.py
@@ -4,14 +4,36 @@ class Card:
    value(int) is the numerical value of the card
    suit(str) is the suit of the card
     """
-    def __init__(self,value,suit):
+    suits = ('Clubs', 'Diamonds', 'Hearts', 'Spades')
+    ranks = ('narf', 'Ace', '2', '3', '4', '5', '6', '7',
+             '8', '9', '10', 'Jack', 'Queen', 'King']
+    
+    def __init__(self, suit=0, rank=0):
     """the __init__function here creates a value for each card and assign a suit
+    Args: 
+        suit(str): suit of the card
+        rank(str): number on the card
+    Returns: N/a
+    Raises: N/A
+    Side Effects: N/A
     """
+        self.suit = suit
+        self.rank = rank
 
     def __str__(self):
     """the __str___ function will return a statement that states the name of the card and its suit
+          >>> print(Card(2, 11))
+          Queen of Hearts
+    Args: self
+    Returns: rank and suit of card
+    Raises: N/A
+    Side Effects: N/A
     """
-        
+        return self.rank + 'of' + self.suit
+    
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()      
         
 class Deck:
     """ Makes a deck of cards and takes a card if there are cards left
