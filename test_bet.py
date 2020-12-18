1f2c0f328495e736cbd26575d0d9eb8fe8e95f34
Wilson Badillo <wilson_badillo23@yahoo.com>
2020-12-18 11:00:09 -0500

    Update to test files

--- test_bet.py
+++ test_bet.py
@@ -1,3 +1,5 @@
+# Wilson pytest code
+
 from project import Player
 import pytest
 
@@ -5,3 +7,17 @@ def test_bet():
     temp = Player(5000, 'George', 4)
     temp.bet(1000)
     assert 4000 == temp.balance
+    temp.bet(1000)
+    assert 3000 == temp.balance
+def test_fold():
+    temp = Player(5000, 'George', 4)
+    temp.foldround = False
+    assert False == temp.fold()
+    temp.foldround = True
+    assert True == temp.fold()
+def test_double_down():
+    temp = Player(5000, 'George', 4)
+    temp.double_down(1000)
+    assert 3000 == temp.balance
+    temp.double_down(1000)
+    assert 1000 == temp.balance
