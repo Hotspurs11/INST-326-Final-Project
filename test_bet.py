55c7e198d811662a4b92cd787ea1321bde4d1928
nayabmahmood <73296023+nayabmahmood@users.noreply.github.com>
2020-12-22 13:49:50 -0500

    Update test_bet.py

--- test_bet.py
+++ test_bet.py
@@ -1,7 +1,8 @@
-#Pytest Code
+"""Tests blackjack project code"""
+from project import Game
 from project import Player
+from project import player_game
 import pytest
-
 def test_bet():
     temp = Player(5000, 'George', 4)
     temp.bet(1000)
@@ -20,3 +21,10 @@ def test_double_down():
     assert 3000 == temp.balance
     temp.double_down(1000)
     assert 1000 == temp.balance
+def test_check_blackjack():
+    p_g = player_game()
+    v_d = p_g.value_display()
+    if p_g.player_hand() == 21:
+        assert check_blackjack() == True, False
+    if p_g.player_hand() == 21:
+        assert check_blackjack() == False, True
